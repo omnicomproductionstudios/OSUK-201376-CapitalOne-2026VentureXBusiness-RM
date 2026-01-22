f16c237ffd8c01a3eaef8ab996b2bc28281d7382
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2026-01-22 17:40:43 +0530

    dfg

---
+++ GSDM-P00114028_VXB_2025Campaign_RM_970x66/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+	"filename":"GSDM-P00114028_VXB_2025Campaign_RM_970x66.html",
+	"width":970,
+	"height":66,
+	"clickTagCount":1,
+  "videos":[{"name":"video1", "ref":"231590/TURW5182100H_P00115797_VXB_GOING_THE_DISTANCE_OLV_15_HD_20251202_H264"}],
+	"expand":{
+		"fullscreen":false,
+		"width":970,
+		"height":250,
+		"indentAcross":0,
+		"indentDown":0
+  }
+});
